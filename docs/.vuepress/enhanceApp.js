4404a95ccdabf0eb4400162f50e4bab2991f076a
sunjunhao <sunjunhao@banggood.com>
2019-10-30 19:44:49 +0800

    测试vuepress组件无效

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,13 @@
+import VueQuarterSelect from '../../src'
 export default({ Vue }) => {
-  import('../../src')
-    .then(module => {
-      const VueQuarterSelect = module.default
-      Vue.use(VueQuarterSelect)
-    })
-    .catch(e => {})
+  Vue.use(VueQuarterSelect)
+  // import('https://unpkg.com/browse/@3scarecrow/vue-quarter-select@0.1.1/dist/vue-quarter-select.esm.js')
+  //   .then(module => {
+  //     console.log('module',module)
+  //     const VueQuarterSelect = module.default
+  //     Vue.use(VueQuarterSelect)
+  //   })
+  //   .catch(e => {})
 
   // import('@laomao800/vue-demo-box')
   //   .then(module => {
