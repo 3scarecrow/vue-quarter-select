e04f55cbbde0b40b136f04f8709af7c82f775742
sunjunhao <sunjunhao@banggood.com>
2019-10-30 14:52:02 +0800

    vuepress rebuild

---
+++ docs/.vuepress/components/demo/disabled.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>默认值 date: {{date}}</div>
+    <VueQuarterSelect v-model="date" minDate="2019-04-10" :max-date="new Date('2020-01-01')"></VueQuarterSelect>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: []
+    }
+  }
+}
+</script>
+
+<style>
+</style>
